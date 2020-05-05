6005a4c8042392df07e4af2b953243396dcdcec2
santirodriguezz <santirodriguezsalinas@gmail.com>
2020-05-05 00:27:11 -0300

    organice todo en fieldsOutfitting'

--- fieldsOutfitting/app.js
+++ fieldsOutfitting/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var homeRouter = require('./routes/home');
+var carritoRouter = require('./routes/carrito');
+
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/home', homeRouter);
+app.use('/carrito', carritoRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
