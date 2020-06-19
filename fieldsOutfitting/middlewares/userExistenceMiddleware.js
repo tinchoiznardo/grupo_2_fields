2bb3f702feb3c8d316bc8fb796e9f56b951501b7
santirodriguezz <santirodriguezsalinas@gmail.com>
2020-06-19 11:04:28 -0300

    arregle login-cookie

--- fieldsOutfitting/middlewares/userExistenceMiddleware.js
+++ fieldsOutfitting/middlewares/userExistenceMiddleware.js
@@ -7,13 +7,8 @@ const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 module.exports = function userExistenceMiddleware (req, res, next) {
     if (!req.session.user){
         next();
-    } else if (!req.cookies.user){
-        const user = users.find((user) => {
-            return user.id == req.cookies.user;
-        });
-        req.session.user = user;
-        next();
     } else {
         return res.redirect('/user/profile');
     };
 };
+
