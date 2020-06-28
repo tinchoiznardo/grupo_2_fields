11a36e1091714552f81e8301f3f6709190b05c3e
tinchoiznardo <tinchoiznardo@hotmail.com>
2020-06-28 17:40:42 -0300

    update de edicion de usuario funcionando

--- fieldsOutfitting/database/config/config.js
+++ fieldsOutfitting/database/config/config.js
@@ -1,10 +1,9 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": null,
     "database": "fields",
     "host": "127.0.0.1",
-    "port": "8889",
     "dialect": "mysql",
     "operatorsAliases": false
   },
