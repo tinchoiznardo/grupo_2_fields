f9a41446dc032b2a6f6d4f391f30b78c8a05987f
tinchoiznardo <tinchoiznardo@hotmail.com>
2020-08-08 19:18:23 -0300

    update api products

---
+++ fieldsOutfitting/controllers/api/productsController.js
@@ -0,0 +1,38 @@
+let fs = require('fs');
+let path = require('path');
+
+// let productsFilePath = path.join(__dirname, './data/products.json');
+// let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+let db = require('../../database/models');
+
+
+const productsController = {
+	// List - Show all products
+	list: async (req, res) => {
+        let products = await db.Product.findAll({
+            attributes: [
+                "id",
+                "name",
+                "price",
+                "image"
+            ]
+        });
+        for (let i = 0 ; i < products.length ;i++) {
+            await products[i].setDataValue ('endpoint', `/api/users/${products[i].id}`)
+        } 
+
+        
+        let answer = {
+            meta: {
+                status:200,
+                total: products.length,
+                url: '/api/users', 
+            },
+            data: products
+            }
+        res.json(answer)
+    },
+}
+
+module.exports = productsController;
--- fieldsOutfitting/database/config/config.js
+++ fieldsOutfitting/database/config/config.js
@@ -1,10 +1,9 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": "Mysqlm4g1c0",
     "database": "fields",
     "host": "127.0.0.1",
-    "port": "8889",
     "dialect": "mysql",
     "operatorsAliases": false
   },
