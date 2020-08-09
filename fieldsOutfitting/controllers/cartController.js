16ef9f0ae7adca71b16b6cefe6d701e01f9ddd8d
santirodriguezz <santirodriguezsalinas@gmail.com>
2020-08-09 11:39:00 -0300

    product  cart

--- fieldsOutfitting/controllers/cartController.js
+++ fieldsOutfitting/controllers/cartController.js
@@ -1,19 +1,38 @@
 let db = require('../database/models');
 
 const cartController = {
-  show: (req, res) => {
+  show: async (req, res) => {
+    const user = req.session.user 
+    const productCart = await ProductCart.findAll({ where: {
+      cart_id: user.cart_id 
+    }});
+    const products = await Product.findAll();
+    
     res.render('cart', { 
-      user: req.session.user 
+      user: req.session.user,
+      products: products,
+      productCart: productCart 
     });
   },
   save: async (req, res) => {
     const user = req.session.user 
-    await db.ProductCart.create({
+    const purchase = {
       product_id: req.body.product_id,
       cart_id: user.cart_id,
       quantity: req.body.quantity,
-  });
+      size: req.body.size_id
+    }
+    await db.ProductCart.create(purchase);
+    const productCart = await ProductCart.findAll({ where: {
+      product_id: req.body.product_id
+    }});
 
-  },}
+    await db.ProductCart.update({
+			stock: productCart.stock - 1,
+      }, { where: {
+        product_id: req.body.product_id
+		}});
+    localStorage.setItem('purchase', purchase)
+    },}
 
 module.exports = cartController
