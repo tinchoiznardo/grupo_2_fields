a1941ebb71498edccafe10e9a7dab3a69c34e662
santirodriguezz <santirodriguezsalinas@gmail.com>
2020-08-12 19:42:26 -0300

    cart

--- fieldsOutfitting/controllers/cartController.js
+++ fieldsOutfitting/controllers/cartController.js
@@ -3,15 +3,23 @@ let db = require('../database/models');
 const cartController = {
   show: async (req, res) => {
     const user = req.session.user 
-    const productCart = await ProductCart.findAll({ where: {
+    const productCart = await db.ProductCart.findAll({ where: {
       cart_id: user.cart_id 
     }});
-    const products = await Product.findAll();
+    const products = await db.Product.findAll()
+    let productsInCart = [];
+
+    // for (purchase of productCart){
+    //   console.log(products.filter(el => el.id == purchase.product_id))
+    //   productsInCart.push(products.filter(el => el.id == purchase.product_id))
+    // }
+
+    console.log(productCart)
     
     res.render('cart', { 
       user: req.session.user,
       products: products,
-      productCart: productCart 
+      productCart: productCart
     });
   },
   save: async (req, res) => {
@@ -20,19 +28,24 @@ const cartController = {
       product_id: req.body.product_id,
       cart_id: user.cart_id,
       quantity: req.body.quantity,
-      size: req.body.size_id
+      size_id: req.body.size_id
     }
+    console.log(purchase, "ASDASDASD")
     await db.ProductCart.create(purchase);
-    const productCart = await ProductCart.findAll({ where: {
+    
+    const productCart = await db.ProductCart.findAll({ where: {
       product_id: req.body.product_id
     }});
 
-    await db.ProductCart.update({
+    await db.ProductSize.update({
 			stock: productCart.stock - 1,
       }, { where: {
         product_id: req.body.product_id
 		}});
-    localStorage.setItem('purchase', purchase)
-    },}
+    // localStorage.setItem('purchase', purchase)
+
+    res.redirect("/")
+    },
+  }
 
 module.exports = cartController
