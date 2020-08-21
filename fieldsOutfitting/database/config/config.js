ac1f47b2d3140f8d3e801c253b511bc699e520dd
tinchoiznardo <tinchoiznardo@hotmail.com>
2020-08-20 21:22:25 -0300

    arrelglos

--- fieldsOutfitting/database/config/config.js
+++ fieldsOutfitting/database/config/config.js
@@ -1,11 +1,10 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
-    "port": "8889",
+    "password": "Mysqlm4g1c0",
+    // "port": "8889",
     "database": "fields",
     "host": "127.0.0.1",
-    "port": "8889",
     "dialect": "mysql",
     "operatorsAliases": false
   },
